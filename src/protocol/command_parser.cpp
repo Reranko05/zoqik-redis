2580d8d57be46839074e1724b8b7ade85704c811
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-05-07 23:25:24 +0530

    notes: added comments command_parser

--- src/protocol/command_parser.cpp
+++ src/protocol/command_parser.cpp
@@ -1,3 +1,36 @@
+/*
+    This file is responsible for tokenizing parsed command strings.
+
+    Input:
+    - A complete command string extracted from the TCP stream parser.
+
+    Example:
+    "SET name Aaditya"
+
+    Output:
+    ["SET", "name", "Aaditya"]
+
+    This layer exists because applications work with structured commands,
+    not raw strings or TCP byte streams.
+
+    We used std::stringstream with space-delimited parsing
+    to split commands into individual tokens.
+
+    This file does NOT handle:
+    - sockets
+    - recv/send
+    - TCP stream parsing
+    - command execution
+    - database state
+
+    Its only responsibility is:
+    command string -> structured token vector
+
+    Current limitation:
+    - values containing spaces are split into multiple tokens
+    - this is acceptable for now and will later be solved properly using RESP
+*/
+
 #include "command_parser.h"
 
 #include <sstream>
