9f1e6b9e0846c3f9f95cb48ae7f9047a3971b59f
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-05-08 00:11:51 +0530

    notes: Added database notes

--- src/storage/database.cpp
+++ src/storage/database.cpp
@@ -1,3 +1,19 @@
+/*
+    Implements in-memory key-value storage layer using unordered_map.
+
+    set() method adds or overwrites key-value pairs in storage.
+
+    get() method uses an iterator to lookup the desired key.
+    If found, it returns the stored value.
+    Otherwise, it returns "Key Not Found".
+
+    get() uses only one hashmap lookup for both validation
+    and value retrieval.
+
+    del() method checks whether the key exists in storage.
+    If found, it erases the key-value pair from storage.
+*/
+
 #include "database.h"
 
 void Database::set(const std::string& key, const std::string& value) {
